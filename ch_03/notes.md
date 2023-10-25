c193240e0a8437dc29a0c3de1e9b7e35ad2c0b28
LuisGilGB <luis.gilgb@gmail.com>
2023-10-26 00:49:25 +0200

    Chapter 3 - Added first route handler with serialized response

--- ch_03/notes.md
+++ ch_03/notes.md
@@ -1,2 +1,38 @@
 # Create your first route handler
 
+In this chapter we implement a simple server with its simple route handler with Warp.
+
+When using Warp, both `warp` and `tokio` crates have to be added to the project.
+
+## Filters
+
+Rote handlers in Warp are implemented with Warp filters.
+
+```rust
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .and(warp::path::end())
+        .and_then(my_route_handler);
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+```
+
+## Route handler
+
+Route handler is a function that takes a request and returns a response. In Warp, route handlers are implemented as
+async functions that return a `Result` of a `warp::Reply` (for the success) or a `warp::Rejection` (for the error
+cases).
+
+```rust
+use warp::Filter;
+
+async fn my_route_handler(name: String) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Hello, {}!", name))
+}
+```
--- ch_03/qa-web-app/src/main.rs
+++ ch_03/qa-web-app/src/main.rs
@@ -1,8 +1,82 @@
+use std::fmt;
+use std::str::FromStr;
+
+use serde::Serialize;
 use warp::Filter;
+
+#[derive(Debug, Serialize)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug, Serialize)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl fmt::Display for Question {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            formatter,
+            "Question: {}, {}, {}, {:?}",
+            self.id, self.title, self.content, self.tags
+        )
+    }
+}
+
+impl fmt::Display for QuestionId {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{}", self.0)
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "No id provided",
+            )),
+        }
+    }
+}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string())),
+    );
+
+    Ok(warp::reply::json(&question))
+}
 
 #[tokio::main]
 async fn main() {
-    let hello_handler = warp::path("hello").map(|| format!("Hello, World!"));
+    let hello_handler = warp::path("health").map(|| format!("Alive"));
 
-    warp::serve(hello_handler).run(([127, 0, 0, 1], 3030)).await;
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
+
+    let routes = get_items.or(hello_handler);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
