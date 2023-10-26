7f5fee428e28f5709c54632809a6580232b519d5
LuisGilGB <luis.gilgb@gmail.com>
2023-10-26 21:56:44 +0200

    Chapter 3 - Added error handling

--- ch_03/notes.md
+++ ch_03/notes.md
@@ -36,3 +36,64 @@ async fn my_route_handler(name: String) -> Result<impl warp::Reply, warp::Reject
     Ok(format!("Hello, {}!", name))
 }
 ```
+
+## Handling errors in Warp
+
+By default, when a request is rejected, Warp returns a 404 response. This can be changed by using the `recover` filter,
+where we can specify a custom error handler that works with the rejections returned by the previous filters.
+
+```rust
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .and(warp::path::end())
+        .and_then(my_route_handler)
+        .recover(handle_rejection);
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+```
+
+But to return a custom error, we need:
+
+1. To create a custom error type.
+2. To implement the `warp::reject::Reject` trait for the custom error type.
+3. Return the custom error type from the route handler.
+
+```rust
+use warp::{Filter, reject::Reject, Reply, Rejection};
+
+#[derive(Debug)]
+struct CustomError;
+
+impl Reject for CustomError {}
+
+async fn my_route_handler(name: String) -> Result<impl Reply, Rejection> {
+    Err(warp::reject::custom(CustomError))
+}
+```
+
+To check what type of error was returned, we can use the `find` method on the `Rejection` type.
+
+```rust
+async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
+    match r.find() {
+        Some(InvalidId) => {
+            Ok(warp::reply::with_status(
+                "No valid id provided",
+                StatusCode::UNPROCESSABLE_ENTITY,
+            ))
+        }
+        _ => {
+            Ok(warp::reply::with_status(
+                "Route not found",
+                StatusCode::NOT_FOUND,
+            ))
+        }
+    }
+}
+```
--- ch_03/qa-web-app/src/main.rs
+++ ch_03/qa-web-app/src/main.rs
@@ -2,7 +2,7 @@ use std::fmt;
 use std::str::FromStr;
 
 use serde::Serialize;
-use warp::Filter;
+use warp::{Filter, http::StatusCode, reject::Reject, Rejection, Reply};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -14,6 +14,9 @@ struct Question {
 
 #[derive(Debug, Serialize)]
 struct QuestionId(String);
+
+#[derive(Debug)]
+struct InvalidId;
 
 impl Question {
     fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
@@ -56,7 +59,9 @@ impl FromStr for QuestionId {
     }
 }
 
-async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+impl Reject for InvalidId {}
+
+async fn get_questions() -> Result<impl Reply, Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
@@ -64,7 +69,31 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
         Some(vec!("faq".to_string())),
     );
 
-    Ok(warp::reply::json(&question))
+    match question.id.0.parse::<i32>() {
+        Err(_) => {
+            Err(warp::reject::custom(InvalidId))
+        }
+        Ok(_) => {
+            Ok(warp::reply::json(&question))
+        }
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    match r.find() {
+        Some(InvalidId) => {
+            Ok(warp::reply::with_status(
+                "No valid id provided",
+                StatusCode::UNPROCESSABLE_ENTITY,
+            ))
+        }
+        _ => {
+            Ok(warp::reply::with_status(
+                "Route not found",
+                StatusCode::NOT_FOUND,
+            ))
+        }
+    }
 }
 
 #[tokio::main]
@@ -76,7 +105,9 @@ async fn main() {
         .and(warp::path::end())
         .and_then(get_questions);
 
-    let routes = get_items.or(hello_handler);
+    let routes = get_items
+        .or(hello_handler)
+        .recover(return_error);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
